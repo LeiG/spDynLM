a2245213d23c4c24b039e8c71b2644971f21bc1b
Lei Gong <lgong0715@gmail.com>
2014-02-17 16:06:52 -0800

    beta test

--- alternativeBM.R
+++ alternativeBM.R
@@ -10,19 +10,58 @@ samples<- cbind(beta.0.samples, beta.samples, sigma.eta.samples, theta.samples)
 
 ## stopping rule
 eps<- 0.05
-jump<- 20
-check<- 2^12
 z<- 1.96
 d<- dim(samples)[2]
-b<- matrix(, 20, d)
-X<- y = sapply(1:a, function(k) return(mean(g(x[((k - 1) * 
-                                                   b + 1):(k * b)]))))
-X<- matrix(, check/sqrt(check), d)
-sapply(0:b, function(j) return(mean((g(x[1:(n - j)]) - 
-                                       mu.hat) * (g(x[(j + 1):n]) - mu.hat))))
+b.size<- c(2^6, 2^6)
+batch<- matrix(, b.size[1], d)
+b.count<- 0 # number of iterations within batch
+batch.count<- 0 # number of batches
+n<- 0 # number of iterations
+tank.std<- matrix(0, 3, d)
+tank.std[1,]<- samples[1,]
 while(1){
-  b.size<- sqrt(check)
-  nbatch<- check/b.size
+  b.count= b.count+1
+  n= n+1
+  batch[b.count,]<- samples[n,]
+  tank.std[2,]<- ((n-1)*tank.std[1,]+batch[b.count,])/n
+  tank.std[3,]<- tank.std[3,]+n*(tank.std[2,]-batch[b.count,])**2/(n-1)
+  tank.std[1,]<- tank.std[2,]
   
+  # check if batch is full
+  if(b.count == b.size[1]){
+    b.count<- 0
+    if(batch.count == 0){
+      tank<- matrix(apply(batch, 2, mean), nrow=1)
+      batch.count<- 1
+    }
+    else{
+      tank<- rbind(tank, matrix(apply(batch, 2, mean), nrow=1))
+      batch.count= batch.count+1
+    }
+  }
   
+  # check every 20/21 batches
+  if((n>=2^12)&&(batch.count>20)&&(!(dim(tank)[1]%%2))){
+    batch.count<- 1
+    b.size[2]<- b.size[1]
+    b.size[1]<- sqrt(n)
+    nbatch<- n/b.size[1]
+    
+    #check if batch size changes
+    if(b[1] != b[2]){
+      batch<- matrix(, b.size[1], d)
+      tank<- t(sapply(split.data.frame(tank, rep(1:(dim(tank)[1]/2), times=rep(2, (dim(tank)[1]/2)))), apply, 2, mean))
+    }
+    
+    tank.mcse<- sqrt(apply((tank-apply(tank, 2, mean))^2, 2, sum)*b.size[1]/((nbatch-1)*n))
+    cond<- 2*z*tank.mcse-eps*sqrt(tank.std[3,]/(n-1))
+    if(all(cond<=0)){
+      ess.new<- (tank.std[3,]/(n-1))/tank.mcse^2*n
+      ess.app<- 4*(z/eps)^2
+      out<- list(n= check, app= ess.app, new= ess.new)
+      write.table(out, "output_origin.txt")
+      write.table(tank.mcse, "mcse_origin.txt")
+      break
+    }
+  }
 }
