24faa11cc86c456211a32c2d324f432d7f863fc8
Lei Gong <lgong0715@gmail.com>
2014-02-26 07:25:33 -0800

    debug

--- originBM.R
+++ originBM.R
@@ -95,8 +95,8 @@ while(1){
     out<- list(n= check, app= ess.app, old= ess.old, new= ess.new)
     write.table(out, paste(eps, "output_origin.txt", sep="_"))
     write.table(mcse, paste(eps, "mcse_origin.txt", sep="_"))
-    write.table(sqrt(std, paste(eps, "sd_origin.txt", sep="_"))
-    write.table(sqrt(X.mean, paste(eps, "mean_origin.txt", sep="_"))
+    write.table(std, paste(eps, "sd_origin.txt", sep="_"))
+    write.table(X.mean, paste(eps, "mean_origin.txt", sep="_"))
     break
   }
   
