d72a3797884f6feb8909c4c95d82f6faff7b3889
Lei Gong <lgong0715@gmail.com>
2014-10-17 09:01:50 -0700

    add flag

--- V2/spBayes_truth_para.R
+++ V2/spBayes_truth_para.R
@@ -61,6 +61,7 @@ m.1 <- spDynLM(mods, data=cbind(y.t,ne.temp[,"elev",drop=FALSE]),
                coords=coords,starting=starting, tuning=tuning, 
                priors=priors, get.fitted =TRUE,cov.model="exponential",
                n.samples=n.samples, n.report=0.1*n.samples)
+print("Generator DONE!")
 samples<- cbind(m.1$p.beta.0.samples, m.1$p.beta.samples, 
                 m.1$p.sigma.eta.samples, m.1$p.theta.samples, 
                 t(m.1$p.u.samples))
